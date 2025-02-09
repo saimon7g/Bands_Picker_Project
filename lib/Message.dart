5883e28873d97216752a6bd5647cd3086d628e21
Nur Uddin Ibne Huda <nuruddin.simon@gmail.com>
2025-02-09 14:40:31 +0600

    4 different pages added

---
+++ lib/Message.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+
+class MessagePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        'Message Page',
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
+
